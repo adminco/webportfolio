39b62d3bf249e21abcb085d3d17e4f37145d44e6
adminco.local <gozy_black@yahoo.com>
2019-01-25 09:00:52 -0500

    made some changes to the about page

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,5 +1,3 @@
-// import Typed from 'typed.js'
-
 // Select DOM elements
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
@@ -20,8 +18,7 @@ $(document).ready(() => {
   const $btn3 = $(".btn3");
   const $btn4 = $(".btn4");
   const $btn5 = $(".btn5");
-  
-  
+
   // Event Handlers
   $btn1.on("click", event => {
     event.currentTarget;
@@ -47,7 +44,6 @@ $(document).ready(() => {
 
 // Set initial state of menu
 let showMenu = false;
-let showDescriptionEvent = false;
 
 function toggleMenu() {
   if (!showMenu) {
@@ -56,7 +52,6 @@ function toggleMenu() {
     menuBranding.classList.add("show");
     menuNav.classList.add("show");
     navItems.forEach(navItem => navItem.classList.add("show"));
-
     showMenu = true;
   } else {
     menuBtn.classList.remove("close"); // this removes a class value
@@ -67,17 +62,17 @@ function toggleMenu() {
     showMenu = false;
   }
 }
-
+// Event Handler to toggle menu navigation
 menuBtn.addEventListener("click", toggleMenu);
 
 // Type JS Animation
 var options = {
   strings: [
     "",
-    'Full Stack^1000 Developer',
-    'Systems Engineer',
-    'IT^1000 Support Analyst',
-    'DevOps^1000 Engineer'
+    "Full Stack^1000 Developer",
+    "Systems Engineer",
+    "IT^1000 Support Analyst",
+    "DevOps^1000 Engineer"
   ],
   typeSpeed: 250,
   startDelay: 0,
@@ -85,12 +80,7 @@ var options = {
   backDelay: 1000,
   loop: true,
   loopCount: Infinity,
-  showCursor: false,
-  // shuffle: true,
-}
+  showCursor: false
+};
 
 var typed = new Typed(smHeading, options);
-
-
-
-
